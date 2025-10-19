2e9a33b02b2b83c3db60a254e39a45d2f8399df3
jin <flatupgym@gmail.com>
2025-10-19 21:50:45 +0900

    fix: Add storageBucket to Firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,22 @@
-// src/lib/firebase.ts
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
-import { getFunctions } from "firebase/functions";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  // ここを環境変数から読み込むように変更！
+  storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
+// Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
 
-export const storage = getStorage(app);
-export const functions = getFunctions(app, "asia-northeast1"); // 東京リージョン
+export { app, db, auth, storage };

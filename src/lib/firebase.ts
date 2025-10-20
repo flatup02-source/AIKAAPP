1a8c04c02199aa59345e0caceac8b886f03bd600
jin <flatupgym@gmail.com>
2025-10-21 05:10:03 +0900

    feat: Configure Cloud Build to inject Firebase secrets

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,10 +7,10 @@ const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  // ここを環境変数から読み込むように変更！
-  storageBucket: process.env.STORAGE_BUCKET,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase

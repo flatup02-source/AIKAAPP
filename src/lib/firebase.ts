2db5998b23405553b0d6f89785a3d70d5683260c
jin <flatupgym@gmail.com>
2025-10-21 11:47:31 +0900

    fix: Correct Firebase config and update image tag

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDDy5_-jv0BQCCFIHyPgXvH7sBjE83mnp4",
+  authDomain: "aikaapp-584fa.firebaseapp.com",
+  projectId: "aikaapp-584fa",
+  storageBucket: "aikaapp-584fa.appspot.com",
+  messagingSenderId: "639286700347",
+  appId: "1:639286700347:web:2216c51a5ebb126b516f1e",
+  measurementId: "G-T9QETENBLZ"
 };
 
 // Initialize Firebase

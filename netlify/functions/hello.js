048435a0ba7f521e0bcc191c78ccbcbd9bb22849
jin <flatupgym@gmail.com>
2025-10-27 22:30:41 +0900

    feat(gcs): switch to Next.js API route for signed URL generation and cleanup Netlify Functions

--- netlify/functions/hello.js
+++
@@ -1,7 +0,0 @@
-// This is a test function for Netlify Function detection.
-exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: `Hello from Netlify Function! Your path: ${event.path}`,
-  };
-};
